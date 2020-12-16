848de568f4c30c5d95a21e1437770d6b3c65e6da
Thomas Hintermeier <hintermeier-t@protonmail.com>
2020-12-16 14:37:52 +0100

    Deployment Correction

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 
+import django_heroku
 import dj_database_url
 from pathlib import Path
 
@@ -147,10 +148,12 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
+STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
